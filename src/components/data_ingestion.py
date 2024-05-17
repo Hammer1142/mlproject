561bbf0d933dce38cf920c6a312ce3ba4ab2572f
Hammer1142 <fabian.hammerschmid@icloud.com>
2024-05-17 17:14:30 +0200

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,6 +8,7 @@ from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+from src.components.model_trainer import ModelTrainerConfig, ModelTrainer
 
 
 @dataclass
@@ -52,7 +53,10 @@ if __name__=="__main__":
     train_data, test_data = obj.initiate_data_ingestion()
     
     data_transfomration = DataTransformation()
-    data_transfomration.initiate_data_transformation(train_data, test_data)
+    train_arr, test_arr,_ = data_transfomration.initiate_data_transformation(train_data, test_data)
+    
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr, test_arr))
 
         
     
